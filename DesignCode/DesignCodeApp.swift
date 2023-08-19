2ca1f516afd60c73a6bd9a3d229c703f0e36d8d8
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2023-08-19 21:54:00 +0530

    Initial Commit

---
+++ DesignCode/DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeApp.swift
+//  DesignCode
+//
+//  Created by Yohan on 2023-08-19.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

bd0f1a9cfff1a34dd91fb88b60caaac4ab053298
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2023-08-20 11:59:24 +0530

    Added state to handle bottom sheet animation

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -12,6 +12,8 @@ struct ContentView: View {
     @State var show = false
     @State var viewState = CGSize.zero // X and Y positions
     @State var showCard = false
+    @State var bottomState = CGSize.zero
+    @State var showFull = false
     
     var body: some View {
         ZStack {
@@ -76,6 +78,10 @@ struct ContentView: View {
                     DragGesture().onChanged { value in
                         self.viewState = value.translation
                         self.show = true
+                        
+                        if self.bottomState.height < -300 {
+                            self.bottomState.height = -300
+                        }
                     }
                         .onEnded { value in
                             self.viewState = .zero
@@ -83,10 +89,35 @@ struct ContentView: View {
                         }
                 )
             
+//            Text("\(bottomState.height)")
+//                .offset(y: -300)
+            
             BottomCardView()
                 .offset(x: 0, y: showCard ? 360 :  1000)
+                .offset(y: bottomState.height)
                 .blur(radius: show ? 20 : 0)
                 .animation(.timingCurve(0.2, 0.8, 0.2, 1, duration: 0.8))
+                .gesture(
+                    DragGesture().onChanged { value in
+                        self.bottomState = value.translation
+                        if self.showFull {
+                            self.bottomState.height += -300
+                        }
+                    }
+                        .onEnded { value in
+                            if self.bottomState.height > 50 {
+                                self.showCard = false
+                            }
+                            if (self.bottomState.height < -100 && !self.showFull) ||
+                                (self.bottomState.height < -250 && self.showFull){
+                                self.bottomState.height = -300
+                                self.showFull = true
+                            } else {
+                                self.bottomState = .zero
+                                self.showFull = false
+                            }
+                        }
+                )
         }
     }
 }

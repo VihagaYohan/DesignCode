691bc3501fdb9a6379e4cf68cde1d027832d0178
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2023-08-22 06:05:31 +0530

    Navigation view and list

---
+++ DesignCode/UpdateList.swift
@@ -0,0 +1,64 @@
+//
+//  UpdateList.swift
+//  DesignCode
+//
+//  Created by Yohan on 2023-08-22.
+//
+
+import SwiftUI
+
+struct UpdateList: View {
+    var body: some View {
+        NavigationView {
+            List(updateData) { update in
+                NavigationLink(destination: Text(update.text)) {
+                    HStack {
+                        Image(update.image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 80, height: 80)
+                            .background(Color.black)
+                            .cornerRadius(20)
+                            .padding(.trailing, 4)
+                        
+                        VStack(alignment: .leading, spacing: 8.0) {
+                            Text(update.title)
+                                .font(.system(size: 20, weight: .bold))
+                            Text(update.text)
+                                .lineLimit(2)
+                                .font(.subheadline)
+                                .foregroundColor(Color.gray)
+                            Text(update.date)
+                                .font(.caption)
+                                .fontWeight(.bold)
+                                .foregroundColor(.secondary)
+                        }
+                    }
+                    .padding(.vertical, 8)
+                }
+            }
+            .navigationBarTitle(Text("Updates"))
+        }
+    }
+}
+
+struct UpdateList_Previews: PreviewProvider {
+    static var previews: some View {
+        UpdateList()
+    }
+}
+
+struct Update: Identifiable {
+    var id = UUID()
+    var image: String
+    var title: String
+    var text: String
+    var date: String
+}
+
+let updateData: [Update] = [
+    Update(image: "Card1", title: "SWIFT UI Advanced", text: "Take your SWIFT UI app to the App store with advanced techniques like API data, packages and CMS", date: "Jan 1"),
+Update(image: "Card2", title: "Webflow", text: "Design and animate a high converting landing page with advanced interactions, payments and CMS", date: "OCT 17"),
+Update(image: "Card3", title: "ProtoPie", text: "Quickly prototype advanced animations and interactions for mobile and Web", date: "AUG 27"),
+Update(image: "Card4", title: "SWIFT UI", text: "Learn how to code custom UI, animations, gestures and components in Xcode 11", date: "JUNE 26"),
+Update(image: "Card5", title: "Framer Playground", text: "Create powerful animations and interactions with the Framer X code editor", date: "JUNE 11")]
